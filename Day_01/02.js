3a79c98bab6281d536abd65b9ff64d6b4b5f3ff6
sandeshbokare111 <114687815+sandeshbokare111@users.noreply.github.com>
2022-10-04 00:12:26 +0530

    Delete Day_01 directory
    delete

--- Day_01/02.js
+++
@@ -1,8 +0,0 @@
-let number = prompt("enter the number to reverse")
-let reverse = 0;
-while (number!=0){
-    reminder = number%10;
-    reverse = (reverse*10)+reminder;
-    number = Math.floor(number/10);
-}
-console.log(reverse);
